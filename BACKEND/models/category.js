4ba9cb50d34cbe1240c591b52512f63e8202fbb0
Islam_blk <147268903+islam-blk@users.noreply.github.com>
2024-04-25 12:50:24 +0100

    Add files via upload

---
+++ BACKEND/models/category.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+const categorySchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required :[true,'you must provide a name'],
+        trim : true
+    },
+})
+
+module.exports = mongoose.model("category",categorySchema)
