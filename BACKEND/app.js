c8d23f9f36338a206ac0438004de4df8a273e117
chamsobouabca <b_bouabca@estin.dz>
2024-04-26 21:48:49 +0200

    init

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,36 +1,30 @@
-require("dotenv").config()
-require('express-async-errors');
+require("dotenv").config();
+require("express-async-errors");
 
+const express = require("express");
+const connectDB = require("./db/connect");
+const app = express();
 
+const notFoundMiddleware = require("./middleware/not-found");
 
-const express = require("express")
-const connectDB = require("./db/connect")
-const app = express()
+const port = 5000;
+app.use(express.json());
 
-const notFoundMiddleware = require('./middleware/not-found');
+const categories = require("./routes/categories");
+app.use("/api/products/categories", categories);
 
-const port = 5000
-app.use(express.json())
-
-const categories = require("./routes/categories")
-app.use("/api/products/categories",categories)
-
-const products = require("./routes/products")
-app.use("/api/products",products)
-
+const products = require("./routes/products");
+app.use("/api/products", products);
 
 app.use(notFoundMiddleware);
 
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`server is listening on port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-
-const  start = async ()=>{
-    try {  
-        await connectDB(process.env.MONGO_URI)
-        app.listen(port,console.log(`server is listening on port ${port}`))
-        
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-start()
+start();
---
+++ BACKEND/routes/userRoute.js
@@ -0,0 +1,21 @@
+const {
+  register,
+  activateAccount,
+  logIn,
+  forgetPassword,
+  activateForgetPassword,
+  logOut,
+  update,
+} = require("../controller/userController");
+//const protectedRoute = require("../middleware/protectedRoute");
+const express = require("express");
+const route = express.Router();
+route.post("/register", register);
+
+route.get("/activateAccount/:token", activateAccount);
+route.post("/logIn", logIn);
+route.post("/forgetPassword", forgetPassword);
+route.post("/activateForgetPassword", activateForgetPassword);
+route.get("/logOut", logOut);
+//route.post("/update", protectedRoute, update);
+module.exports = route;
